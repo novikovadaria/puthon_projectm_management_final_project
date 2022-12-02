74bd48ebfb5e5f754c8f9d3df4592690482f8a85
Daria N <107907983+novikovadaria@users.noreply.github.com>
2022-12-02 15:01:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,49 +1,39 @@
-# Дана функция f(x) = -8x^2+3x+17
-
-# 1 Опоределить корни
-
-from sympy import *
-
-x = Symbol('x')
-func=-8*x**2+3*x+17
-y=solve(func)
-x1=float(y[0])
-x2=float(y[1])
-print(x1,x2)
-
-# 2 Найти интервалы, на которых функция возрастает
-
-fd=diff(func)
-print(solve(0<fd))
-
-# 3 Найти интервалы, на которых функция убывает
-
-print(solve(fd<0))
-
-# 4 Построить график
-
+import numpy as np
 import matplotlib.pyplot as plt
-list_y=[]
-for i in range(-5,6):
-    x=i
-    y=-8*x**2+3*x+17
-    list_y.append(y)
-print(list_y)
-plt.plot(range(-5,6),[0,0,0,0,0,0,0,0,0,0,0])
-plt.plot(range(-5,6),list_y)
 
-# 5 Вычислить вершину
+# f(x) = -12x^4*sin(cos(x)) - 18x^3+5x^2 + 10x - 30
 
-corni=solve(fd)
-top=corni[0]
-x=top
-y=-8*x**2+3*x+17
-print(top,y)
+a, b, c, e, k = -12, -18, 5, 10, -30
+x = np.arange(-100, 100, 0.001)
+x_cut = np.arange(-58, -54, 0.001)
+def func(x):
+    function = a*np.sin(np.cos(x))*x**4 + b*x**3 + c*x**2 + e*x + k
+    return function
 
-# 6 Определить промежутки, на котором f > 0
+root = -58
+for i in np.arange(-58, -54, 0.001):
+    if np.absolute(func(root))>np.absolute(func(i)):
+        root = i
 
-solve(0<func)
+min_func = min(func(x_cut))
+def extr_func(x, min_func):
+    extremum_function = a*np.sin(np.cos(x))*x**4 + b*x**3 + c*x**2 + e*x + k - min_func
+    return  extremum_function
 
-# 7 Определить промежутки, на котором f < 0
+extr_x_cut = -58
+for i in np.arange(-58, -54, 0.001):
+    if np.absolute(extr_func(extr_x_cut, min_func))>np.absolute(extr_func(i, min_func)):
+        extr_x_cut = i
 
-solve(func<0)
+x_range_down = np.arange(-58, extr_x_cut, 0.001)
+x_range_up = np.arange(extr_x_cut, -54, 0.001)
+plt.title(f'Корень функции на участке (-58; -54): {round(root, 2)}')
+plt.xlabel('Ось X')
+plt.ylabel('Ось Y')
+plt.grid()
+plt.plot(x, func(x), 'g')
+plt.plot(x_range_down, func(x_range_down), 'r', label="Убывание")
+plt.plot(x_range_up, func(x_range_up), 'b', label="Возрастание")
+plt.text(extr_x_cut, func(extr_x_cut) + 30, f'Вершина функции x = {round(extr_x_cut, 2)}')
+plt.legend()
+plt.show()
